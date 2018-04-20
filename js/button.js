ea4218984044763224b5fdafb5488450478e98ff
Kate <35233028+KateMostepan@users.noreply.github.com>
2018-04-20 18:49:39 +0300

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,11 @@
+<script>
+function showText (el) {
+	if(el.previousElementSibling.clientHeight === 100) {
+		el.previousElementSibling.style.height = "100%";
+		el.innerHTML = "Згорнути...";
+	} else {
+		el.previousElementSibling.style.height = "50px";
+		el.innerHTML = "Більше...";
+	}
+}
+</script>
